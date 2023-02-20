12932d68ba7d85ab042fac44cc70b9246de34abc
Nikhil Raikar <nikhilraikar@Nikhils-MacBook-Pro.local>
2023-02-20 11:05:39 +0530

    git commit number 2

--- src/test/java/com/nikhilprojects/turtlemintclone/TurtlemintCloneApplicationTests.java
+++ src/test/java/com/nikhilprojects/turtlemintclone/TurtlemintCloneApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TurtlemintCloneApplicationTests {
 
     @Test
-    void contextLoads() {
+    void itShouldGenerateId() {
     }
 
 }
